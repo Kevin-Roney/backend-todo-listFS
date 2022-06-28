8a06be39f9e0008f0d5cb045d49d3ed5101b28ce
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-28 15:42:35 -0700

    wrote test for adding todo list item

---
+++ __tests__/todos.test.js
@@ -0,0 +1,51 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'winston@example.com',
+  password: '123456',
+};
+
+const mockUser2 = {
+  email: 'kevin@example.com',
+  password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('items', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('POST /api/v1/items creates a new shopping item with the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+
+    const resp = await agent.post('/api/v1/items')
+      .send({
+        todo: 'buy milk',
+        completed: false
+      });
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      todo: 'buy milk',
+      user_id: user.id,
+      completed: false,
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
